de74b9dfcb9cd175adda1907b9ac6e4e9eaa7d36
Luiz Amorim <luiz.neto@montreetecnologia.com.br>
2026-08-18 09:57:20 -0300

    style: organiza imports do teste

--- src/biblioteca/ControladorBibliotecaTest.java
+++ src/biblioteca/ControladorBibliotecaTest.java
@@ -1,14 +1,9 @@
 package biblioteca;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
+import biblioteca.domain.TipoMembro;
 import org.junit.Test;
 
-import biblioteca.domain.TipoMembro;
+import static org.junit.Assert.*;
 
 public class ControladorBibliotecaTest {
 
